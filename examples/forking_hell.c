8be99cb513755015b362d4bdf27fc971a8702104
Erik Lange <erik.lange@fasttvnet.dk>
2023-10-08 09:44:51 +0200

    adds example forking_hell

---
+++ examples/forking_hell.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	printf("Welcome! (pid:%d)\n", (int)getpid());
+
+	int rc = fork();
+
+	if (rc < 0)
+	{
+		fprintf(stderr, "fork failed\n");
+		exit(1);
+	}
+	else if (rc == 0)
+	{
+		printf("Hello! Here rc = %d and pid = %d.\n", (int)rc, (int)getpid());
+	}
+	else
+	{
+		printf("Hello! Here rc = %d and pid = %d\n", (int)rc, (int)getpid());
+	}
+	return 0;
+}
---
+++ laboratory/experiments/experiment1.md
@@ -0,0 +1,3 @@
+try running forking_hell.c
+Can you guess what is going on?
+Do you get the same output every time?
