4955b1942ed30570e0df1c7fa2ad82e03c423938
Erik Lange <eriklange.cph@proton.me>
2026-01-02 20:04:04 +0000

    better structure

--- src/lib/lib.c
+++ src/lib/lib.c
@@ -40,9 +40,6 @@ int builtin_command(char **argv) {
   if (!strcmp(argv[0], "quit")) {
     exit(0);
   }
-  if (!strcmp(argv[0], "&")) {
-    return 1;
-  }
   return 0;
 }
 
@@ -63,39 +60,41 @@ void prepare(char *cmdline, char **argv, int *bg) {
   *bg = parseline(buf, argv);
 }
 
-void exec(char **argv) {
-  setpgid(getpid(), getpid());
-  if (execve(argv[0], argv, environ) < 0) {
-    printf("%s: Command not found.\n", argv[0]);
-    exit(0);
+pid_t exec(char **argv) {
+  pid_t pid = Fork();
+  if (pid == 0) {
+    setpgid(getpid(), getpid());
+    if (execve(argv[0], argv, environ) < 0) {
+      printf("%s: Command not found.\n", argv[0]);
+      exit(0);
+    }
+  } else {
+    return pid;
   }
+  
 }
 
 void eval(char *cmdline) {
   char *argv[MAXARGS];
   int bg;
-  pid_t pid;
 
   prepare(cmdline, argv, &bg);
 
-  if (argv[0] == NULL) {
+  if (argv[0] == NULL || builtin_command(argv)) {
     return;
   }
+  
+  pid_t pid = exec(argv);
 
-  if (!builtin_command(argv)) {
-    if ((pid = Fork()) == 0) {
-      exec(argv);
+  if (!bg) {
+    set_fg_pgid(pid);
+    int status;
+    if (waitpid(pid, &status, 0) < 0) {
+      unix_error("waitfg: waitpid error");
     }
-
-    if (!bg) {
-      set_fg_pgid(pid);
-      int status;
-      if (waitpid(pid, &status, 0) < 0) {
-        unix_error("waitfg: waitpid error");
-      }
-    } else {
-      printf("%d %s", pid, cmdline);
-    }
+  } else {
+    printf("%d %s", pid, cmdline);
   }
+  
   return;
 }
